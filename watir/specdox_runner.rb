4d47812900edf69078f60c3d5d8fc6bf31d0faae
marekj <marekj.com@gmail.com>
2010-08-18 17:06:49 -0500

    old stuff add

--- watir/specdox_runner.rb
+++ watir/specdox_runner.rb
@@ -1,4 +1,4 @@
-
+require 'watir/ie'
 require 'watir/testcase' #test unit from watir for sequential
 
 require 'spec/expectations' # rspec
@@ -10,11 +10,23 @@ end
 
 
 class TestTest < Watir::TestCase
+	@@ie = nil
+
+	def browser
+		@@ie ||=Watir::IE.attach(:url, /.*/)
+	end
+
   def test_1
+		browser.goto "http://watir.com"
     2.should == 2
   end
+
   def test_2
     'hello'.should_not == 'bye'
+		verify_match "Web Application Testing in Ruby", browser.text
+	  verify browser.title == 'Watir'
+		verify_equal browser.title, 'Watir'
+		verify_match 'watir.com', browser.link(:text, 'Home').href
   end
 end
 

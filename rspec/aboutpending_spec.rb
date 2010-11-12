cc99ec63f6bc618e120292edc32e1be2028a6d5e
marekj <marekj.com@gmail.com>
2010-11-12 07:31:09 -0600

    review examples of rspec usage

--- rspec/aboutpending_spec.rb
+++ rspec/aboutpending_spec.rb
@@ -2,18 +2,24 @@ require 'spec/autorun'
 
 
 describe "pending example" do
-  it 'has pending block that fails' do
+  it 'pend on failure' do
     pending 'pending when block fails expectation' do
       1.should == 2
     end
   end
-  it 'has pending block that throws exception' do
+  it 'pend on exception' do
     pending "pending when block throws exception" do
       "bla".should == foo
     end
   end
 
-  it "has pending block that passes" do
+  it 'pend on error' do
+    pending 'block errors' do
+      z.should be_true
+    end
+  end
+
+  it "pend with passing example should report FIXED and fail" do
     pending 'pending should fail when block passes' do
       1.should == 1
     end
@@ -23,6 +29,8 @@ describe "pending example" do
     begin
       always_return_green
     rescue
+      1.should == 1
+      puts "hello. I am after rescue"  # this will execute
     end
   end
 

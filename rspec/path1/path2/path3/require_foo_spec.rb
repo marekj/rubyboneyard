d10034de290ed00a3d2f598b35196aed995e3dc2
marekj <marekj.com@gmail.com>
2011-02-17 08:58:26 -0600

    notes on require relative paths

--- rspec/path1/path2/path3/require_foo_spec.rb
+++ rspec/path1/path2/path3/require_foo_spec.rb
@@ -16,6 +16,7 @@ describe "diff require foo file from path3" do
   specify "this dir loads only once (no relative path generated)" do
     r File.dirname(__FILE__) + '/foo'
     r File.expand_path('../foo', __FILE__)
+    # this illustrates that relative path here is ok when we require file in the same dir we are in
     $x.should == ['foo3']
   end
 
@@ -23,7 +24,8 @@ describe "diff require foo file from path3" do
     r File.dirname(__FILE__) + '/../foo'
     r File.expand_path('../../foo', __FILE__)
 
-
+    # to illustrate the require statement where the path is relative
+    # it ends up loading the same file twice because the path to each file is uniq
     $x.should == ["foo2", "foo2"] #see redefined constant FOO warning
   end
 
@@ -31,6 +33,8 @@ describe "diff require foo file from path3" do
     r File.expand_path('path4/path5/foo', File.dirname(__FILE__))
     r File.expand_path('path4/foo', File.dirname(__FILE__))
 
+    # to illustrate the relative path in require
+    # it reloads the same file twice since each time path is not uniq
     $x.should == ["foo5", "bar4", "foo4", "bar4"]
   end
 
